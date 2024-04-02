f1821d84ad4591bc89cacfb843cba16ee02a6bc5
lixie19@ios.ac.cn <lixie19@ios.ac.cn>
2024-04-02 17:49:23 +0800

    add llm

--- extract_commit_infos.py
+++ extract_commit_infos.py
@@ -1,37 +1,73 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from lxml import etree
+import os, sys
 import requests
 
 bro = webdriver.Chrome()
 initial_url = "https://github.com/torvalds/linux/commits/master/?author=torvalds"
 brief_xpath = "//div[@data-testid=\"listview-item-title-container\"]//h4//span//a"
 nxt_button_xpath = "//a[@data-testid=\"pagination-next-button\"]"
-changed_file_name_xpath = "//*[@class=\"Link--primary Truncate-text\"]"
+changed_file_name_xpath = "//copilot-diff-entry"
 commit_id = 0
+
+commit_records = []
+diff_file_list = []
 def bro_get_page(curr_depth, url=initial_url):
-    if curr_depth > 10:
+    global commit_id
+    global diff_file_list
+    if curr_depth > 0:
         return
     print("########## PAGE " + str(curr_depth) + " CONTENT ############")
     bro.get(url)
     brief_list = bro.find_elements(By.XPATH, brief_xpath)
     for item in brief_list:
+        commit_id = commit_id + 1
+        temp_str = ""
         print("------------------- commit id " + str(commit_id))
         print(item.get_attribute("title"))
+        temp_str += "------------------- commit id " + str(commit_id) + "\n"
+        temp_str += item.get_attribute("title") + "\n"
         content_bro = webdriver.Chrome()
         content_href = item.get_attribute("href")
         content_bro.get(content_href)
-        changed_files_titles = content_bro.find_elements(By.XPATH, changed_file_name_xpath)
+        changed_files_blocks = content_bro.find_elements(By.XPATH, changed_file_name_xpath)
         print("------------------- changed files:")
-        for changed_title in changed_files_titles:
-            print(changed_title.text)
+        temp_str += "------------------- changed files:\n"
+        for changed_title in changed_files_blocks:
+            data_file_path = changed_title.get_attribute("data-file-path")
+            print(data_file_path)
+            temp_str += data_file_path + "\n"
+            diff_file_list.append(temp_str)
+        commit_records.append(temp_str)
     nxt_btn = bro.find_element(By.XPATH, nxt_button_xpath)
     nxt_btn_href = nxt_btn.get_attribute("href")
     bro_get_page(curr_depth + 1, nxt_btn_href)
-
-
-
+    
+def extract_commit_info_from_local_git():
+    os.system("cd ./commit_files && rm commit_*")
+    os.system("cd linux && git log -n 10 > ../gitlogs.txt")
+    gitlogs_f = open("./gitlogs.txt", "r+")
+    curr_commit_info = ""
+    commit_num = ""
+    for line in gitlogs_f.readlines():
+        if line.startswith("commit"):
+            if curr_commit_info == "" or commit_num == "":
+                pass
+            else:
+                os.mknod("./commit_files/commit_" + commit_num + ".txt")
+                write_f = open("./commit_files/" + "commit_" + commit_num + ".txt", "w+")
+                write_f.write(curr_commit_info)
+                write_f.write("diff files:\n")
+                os.system("cd linux && git log -p -1 " + commit_num + " >> " + "../commit_files/commit_" + commit_num + ".txt")
+                write_f.close()
+            curr_commit_info = ""
+            commit_num = line.split("commit")[1].strip()
+        curr_commit_info += line
+    gitlogs_f.close()
 if __name__ == "__main__":
     init_depth = 0
-    bro_get_page(init_depth)
+    # bro_get_page(init_depth)
+    # print(diff_file_list)
+    extract_commit_info_from_local_git()
     
---
+++ llm_caller.py
@@ -0,0 +1,83 @@
+import openai
+import os, re
+import sys
+
+global_model = "gpt-4-0125-preview"
+# chat interface 
+class chat_interface:
+    def __init__(self) -> None:
+        self.msg_list = []
+
+    def show_conversations(self):
+        print("------------------------------------- conversations")
+        for msg in self.msg_list:
+            if msg['role'] == 'user':
+                print("USER ================== BEGIN")
+                print(msg['content'])
+                print("USER ================== END")
+            else:
+                print("CHATGPT ================== BEGIN")
+                print(msg['content'])
+                print("CHATGPT ================== END")
+        print("------------------------------------- conversations end")
+    
+
+    def set_up_aiproxy_configs(self):
+        openai.api_key = "sk-5nga6ZRm5D87QSytGYlw9jIhItjhnPqxeUoUfRuAJAam87zt"
+        openai.api_base = "https://api.aiproxy.io/v1"
+
+    # reserved for latter if key for openai can be obtained, currently we are using the aiproxy
+    # aiproxy is not free
+    def set_up_default_configs(self):
+        openai.api_key = "sk-5nga6ZRm5D87QSytGYlw9jIhItjhnPqxeUoUfRuAJAam87zt"
+        openai.api_base = "https://api.aiproxy.io/v1"
+
+
+    def ask_question_and_record(self, content):
+        self.msg_list.append({"role": "user", "content": content})
+        res = openai.ChatCompletion.create(
+            model=global_model,
+            messages=[{"role": "user", "content": content}]
+        )
+        answer = res.choices[0].message
+        self.msg_list.append(answer)
+        return answer
+    
+    def set_role_and_ask(self, content):
+        role_message = "You will help me with the commit change of Linux kernel analyzing. \n"
+        role_message += "Each time you will first be given a text containing the commit change description in Github, then the file changed will also be deliverred to you.\n"
+        role_message += "You should first analyze the possible functions that have been changed by the commit, then analyze the syscalls that have been changed by these functions\n"
+        role_message += "If the commit is not about core functionality or the commit cannot influence basic behavior of kernel syscalls, please ignore it\n"
+        role_message += "The final outcome of your answer should follow the format below:\n"
+        role_message += "CHATBEGIN\n"
+        role_message += "IS_CORE_FUNC: YES/NO\n (\"YES\" when the commit is about core functionality that can be tested through syscalls)"
+        role_message += "ANALYSIS: [a sentence of analysis description here]"
+        role_message += "SYSCALL PROGRAM IN C:\n"
+        role_message += "[C Program]\n"
+        role_message += "CHATEND\n"
+        role_message += "In the above format, [C Program] should be in the following format:\n"
+        role_message += "INCLUDES: \n#include <xxx.h>\n #include <xxx.h>"
+        role_message += "PROGRAM: \n[program segment in main without main function body]\n"
+        role_message += '\n'
+        role_message += "Do not use ``` to surround the code, the above code should be generated completely with you only modify the program by\
+                         inserting the include files and syscall programs at CHATGPT INSERT ...\n"
+        message_list = []
+        message_list.append({"role": "user", "content": role_message})
+        message_list.append({"role": "user", "content": content})
+        res = openai.ChatCompletion.create(
+            model=global_model,
+            messages=message_list
+        )
+        answer = res.choices[0].message
+        # print(answer)
+        return answer
+  
+if __name__ == '__main__':
+    commit_info = ""
+    commit_file = open("./commit_files/commit_18685451fc4e546fc0e718580d32df3c0e5c8272.txt", "r+")
+    for line in commit_file.readlines():
+        commit_info += line
+    interface = chat_interface()
+    interface.set_up_aiproxy_configs()
+    answer = interface.set_role_and_ask(commit_info)
+    print(answer.content)
